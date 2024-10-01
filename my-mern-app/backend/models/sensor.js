0dc287ec407d3cc70fcaee81e34a7924f546de08
sethumadhavan7 <chandrabosechandrabose9@gmail.com>
2024-10-02 00:39:45 +0530

    Update sensor.js

--- my-mern-app/backend/models/sensor.js
+++ my-mern-app/backend/models/sensor.js
@@ -1,11 +1,9 @@
-// models/Sensor.js
 const mongoose = require('mongoose');
 
 const sensorSchema = new mongoose.Schema({
     pulse: { type: Number, required: true },
     temperature: { type: Number, required: true },
     humidity: { type: Number, required: true },
-    createdAt: { type: Date, default: Date.now } // Optional: timestamp for readings
-});
+}, { timestamps: true });
 
 module.exports = mongoose.model('Sensor', sensorSchema);
